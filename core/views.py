e3b7da644e461dfbc30395c2caae1e9d71c78ead
Adarsh <4d4r5h.5@gmail.com>
2016-09-20 18:21:53 +0530

    Post update

--- core/views.py
+++ core/views.py
@@ -3,8 +3,8 @@ from .models import Post
 from django.utils import timezone
 
 def main_page(request):
-	posts  = Post.objects.all().order_by('published_date')
-	return render(request, 'core/main.html', {'posts' : posts})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'core/main_post.html', {'posts' : posts}) #Displaying Post On Main Page
 
 def home(request):
 	return render(request, 'core/home.html', {})
