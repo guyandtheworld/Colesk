cf4a7712cc973da1c2ef356d5e0d7e890115fc24
Adarsh <4d4r5h.5@gmail.com>
2016-09-22 06:09:25 +0530

    Links updates

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [ url(r'^$', views.main_page, name = 'main_page'),
     			url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-				url(r'^home/$', views.home, name = 'home'),
-				url(r'^you/$',views.you, name = 'you'),
+				url(r'^networks/$', views.networks, name = 'networks'),
+				url(r'^you/$',views.profile, name = 'profile'),
+				url(r'^feed/$', views.my_feed, name = 'my_feed')
 			]
--- core/views.py
+++ core/views.py
@@ -3,15 +3,18 @@ from .models import Post
 from django.utils import timezone
 
 def main_page(request):
-	posts = Post.objects.filter(published_date__lte = timezone.now())
-	return render(request, 'core/main_post.html', {'posts' : posts}) #Displaying Post On Main Page
+	posts = Post.objects.all()
+	return render(request, 'core/main_post.html', {'posts' : posts[::-1]}) #Displaying Post On Main Page
 
-def post_detail(request, pk=None):
+def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
-	return render(request, 'core/p_detail.html', {'post' : post})
+	return render(request, 'core/post_detail.html', {'post' : post})
 
-def home(request):
-	return render(request, 'core/home.html', {})
+def networks(request):
+	return render(request, 'core/networks.html', {})
 
-def you(request):
+def profile(request):
 	return render(request, 'core/profile.html', {})		
+
+def my_feed(request):
+	return render(request, 'core/my_feed.html', {})	
