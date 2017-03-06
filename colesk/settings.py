d20d91fcae255ad7c62cd1ffe5ee17d5d86f78f6
Adarsh S <4d4r5h.5@gmail.com>
2017-03-07 00:09:31 +0530

    Style login and feeds page

--- colesk/settings.py
+++ colesk/settings.py
@@ -127,4 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = PROJECT_DIR.parent.child('staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child('static'),)
